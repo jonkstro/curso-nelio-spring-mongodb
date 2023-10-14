4f3bea1d594858343312464471ddec10d5616304
Jonas Castro <jonascastro.dev@gmail.com>
2023-10-14 09:32:56 -0300

    Criando entidade POST com USER aninhado

---
+++ src/main/java/com/udemy/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.udemy.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.udemy.workshopmongo.model.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
